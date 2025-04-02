1f2d6dd0caa2891b107393cc5cfc23003576de8b
Atulya Bist <atulya2109@gmail.com>
2025-04-02 01:05:10 -0700

    refactors app-sidebar component into ui folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@ims/components/theme-provider";
-import { SidebarProvider, SidebarTrigger } from "@ims/components/ui/sidebar";
+import { SidebarProvider } from "@ims/components/ui/sidebar";
 import App from "next/app";
-import { AppSidebar } from "@ims/components/app-sidebar";
+import { AppSidebar } from "@ims/components/ui/app-sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
